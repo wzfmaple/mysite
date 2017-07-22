2c13c2765d3f61b2653dbcecc27a9d8c9e1578db
wzfmaple <939792298@qq.com>
2017-07-22 15:23:14 +0800

    vote browser

---
+++ polls/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'polls'
+
+urlpatterns = [
+    # url(r'^$', views.index,name='index'),
+    # url(r'^(?P<question_id>[0-9]+)/$', views.detail,name='detail'),
+    # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='result'),
+    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultView.as_view(), name='result'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+]
